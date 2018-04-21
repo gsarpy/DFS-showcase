53672b39103b0910d046d22da83734a7e457f2aa
Greg Sarpy <evsoul@evair.local>
2018-04-21 15:22:33 -0700

    added routed

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
-let express = require('express');
-let app = express();
-let path = require('path');
-const routes = require('./application/routes');
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 app.set('views', path.join(__dirname, '/application/views'));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  var appName = "AppJam+";
+  res.render('index', {
+    appName: appName
+  });
 });
 
-app.get('/', routes);
-
 app.get('/event/:id', (req, res) => {
-  res.render('pages/event');
+  let eventID = req.params.id;
+  res.render('pages/event', {
+    eventID: eventID
+  });
 });
 
 app.get('/event/:id/judge/:j_id', (req, res) => {
+  eventID = req.param.id;
+  judgeID = req.param.j_id;
   res.render('pages/judge');
 });
 
 app.get('/event/:id/mentor/:m_id', (req, res) => {
-  res.render('pages/mentor');
+  res.render('pages/mentor', {
+    eventID: req.param.id,
+    mentorID: req.param.id
+  });
 });
 
 app.get('/admin', (req, res) => {
-  res.render('pages/admin/index');
+  res.render('pages/admin/index', {});
 });
 
 
