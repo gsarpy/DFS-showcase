af17d918d5f5a525a8f1a276d8ed2c970d446016
Greg Sarpy <evsoul@evair.local>
2018-04-21 14:25:02 -0700

    oh boy, more continued setup

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-const express = require('express');
-const app = express();
+let express = require('express');
+let app = express();
+let path = require('path');
+
+app.set('views', path.join(__dirname, '/application/views'));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('views/pages/index');
+  res.render('index');
+});
+
+app.get('/event/:id', (req, res) => {
+  res.render('pages/event');
+});
+
+app.get('/event/:id/judge/:j_id', (req, res) => {
+  res.render('pages/judge');
+});
+
+app.get('/event/:id/mentor/:m_id', (req, res) => {
+  res.render('pages/mentor');
+});
+
+app.get('/event/:id/admin/:admin_id', (req, res) => {
+  res.render('pages/admin');
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.get('/admin', (req, res) => {
-  res.render('views/pages/admin/index');
+  res.render('pages/admin/index');
 });
 
 let PORT = 8080;
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log("Showcase app is up and running on port ", PORT);
 });
