a46567335ed518d96fddadeb30bb546495ceb604
Greg Sarpy <evsoul@evair.local>
2018-04-21 14:54:59 -0700

    more templates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 let path = require('path');
+const routes = require('./application/routes');
 
 app.set('views', path.join(__dirname, '/application/views'));
 
@@ -9,6 +10,8 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/', routes);
 
 app.get('/event/:id', (req, res) => {
   res.render('pages/event');
@@ -22,20 +25,12 @@ app.get('/event/:id/mentor/:m_id', (req, res) => {
   res.render('pages/mentor');
 });
 
-app.get('/event/:id/admin/:admin_id', (req, res) => {
-  res.render('pages/admin');
-});
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
 app.get('/admin', (req, res) => {
   res.render('pages/admin/index');
 });
 
-let PORT = 8080;
 
+let PORT = 8080;
 app.listen(PORT, () => {
   console.log("Showcase app is up and running on port ", PORT);
 });
