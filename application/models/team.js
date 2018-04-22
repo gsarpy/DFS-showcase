535d1a5ab9c8722c924f31a4424385c1236a93d0
Greg Sarpy <evsoul@evair.local>
2018-04-21 18:16:58 -0700

    added team schema

---
+++ application/models/team.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+
+let Team = new mongoose.Schema({
+  mentor: {
+    type: String,
+    trim: true
+  },
+  score: {
+    type: Integer,
+  },
+  school_name: {
+    type: String,
+    trim: true,
+  }
+});
+
+let TeamSchema = mongoose.model('Team', TeamSchema);
+module.exports = TeamSchema;
